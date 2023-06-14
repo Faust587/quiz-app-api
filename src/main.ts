d996a3dbe9e6df93df2ff0c02723547a436ace1d
Yura Synkulych <kri3g0vec@gmail.com>
2023-06-14 18:45:25 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,11 @@ async function bootstrap() {
   });
   app.use(cookieParser(process.env.JWT_SECRET_KEY));
   app.enableCors({
-    origin: ["http://141.145.214.106:80", "https://faust587.github.io", "https://github.com"],
+    origin: ['http://localhost:3000',
+      'https://faust587.github.io:80',
+      'https://faust587.github.io:443',
+      'https://faust587.github.io',
+    ],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   });
