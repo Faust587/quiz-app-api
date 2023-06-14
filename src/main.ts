db2d5ee1e503c0b1daa77ba644d8835bf2a11dd4
Yura Synkulych <kri3g0vec@gmail.com>
2023-06-14 14:10:53 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   });
   app.use(cookieParser(process.env.JWT_SECRET_KEY));
   app.enableCors({
-    origin: 'https://faust587.github.io/quiz-generator-front-end/',
+    origin: 'https://faust587.github.io',
     credentials: true,
   });
   app.useStaticAssets(join(__dirname, '..', 'public'), {
