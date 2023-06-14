4550bdbcf3089eb822371f8d354c20be36345a8e
Yura Synkulych <kri3g0vec@gmail.com>
2023-06-14 16:12:14 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   });
   app.use(cookieParser(process.env.JWT_SECRET_KEY));
   app.enableCors({
+    origin: ["http://141.145.214.106", "https://faust587.github.io/quiz-generator-front-end/"],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   });
