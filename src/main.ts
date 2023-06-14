13d9f83dbbf2c9be96064803f3463b6fee569bb0
Yura Synkulych <kri3g0vec@gmail.com>
2023-06-14 15:27:41 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -12,8 +12,7 @@ async function bootstrap() {
   app.enableCors({
     origin: 'https://faust587.github.io',
     credentials: true,
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
-    allowedHeaders: '*',
+    preflightContinue: true.
   });
   app.useStaticAssets(join(__dirname, '..', 'public'), {
     prefix: '/public/',
